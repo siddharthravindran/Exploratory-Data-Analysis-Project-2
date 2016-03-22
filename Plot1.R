e3cdfb97f897df6521c93c502b1d7b97c068b0d1
siddharthravindran <siddharthravindran824@gmail.com>
2016-03-22 09:41:05 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,6 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+> png('plot1.png')
+> TotalEmissionsPerYear <- aggregate(Emissions ~ year, NEI, sum)
+> barplot(height=TotalEmissionsPerYear$Emissions, names.arg = TotalEmissionsPerYear$year, xlab = "years", ylab = "Emissions")
+> dev.off()
